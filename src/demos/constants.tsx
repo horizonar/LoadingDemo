295e7c755fdea0655c5cd4e8903d88c30ebdf99f
Gu Xiang <gavinxgu@gmail.com>
2022-08-03 14:14:30 +0800

    feat: 增加 html_rotate_html_arc 例子

---
+++ src/demos/constants.tsx
@@ -0,0 +1,25 @@
+import { keyframes } from "@emotion/react";
+
+export const DEFAULT_SIZE = 60;
+export const DEFAULT_STROKE_WIDTH = 5;
+export const DEFAULT_COLOR = "#3370ff";
+export const colors = [
+  "#3370ff",
+  "#14c0ff",
+  "#00d6b9",
+  "#34c724",
+  "#b3d600",
+  "#ffc60a",
+  "#ff8800",
+  "#f54a45",
+  "#f01d94",
+  "#d136d1",
+  "#4954e6",
+  "#7f3bf5",
+];
+
+export const kfRotate = keyframes`
+to {
+  transform: rotate(360deg);
+}
+`;
