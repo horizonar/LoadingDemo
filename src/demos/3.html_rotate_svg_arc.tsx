a93db1ab339ff615c33440a6f46db1e44739ec44
Gu Xiang <gavinxgu@gmail.com>
2022-08-04 09:39:01 +0800

    feat: 增加 html_rotate_svg_arc 例子

--- src/demos/3.html_rotate_svg_arc.tsx
+++ src/demos/3.html_rotate_svg_arc.tsx
@@ -1,3 +1,107 @@
+/** @jsxImportSource @emotion/react */
+import { css, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+import {
+  colors,
+  DEFAULT_COLOR,
+  DEFAULT_SIZE,
+  DEFAULT_STROKE_WIDTH,
+  kfRotate,
+} from "./constants";
+
+const Container = styled.div<{
+  size?: number;
+}>(({ size = DEFAULT_SIZE }) => ({
+  position: "relative",
+  width: size,
+  height: size,
+}));
+
+const Arc = ({
+  className,
+  size = DEFAULT_SIZE,
+  strokeWidth = DEFAULT_STROKE_WIDTH,
+  color = DEFAULT_COLOR,
+  deg = 45,
+}: {
+  className?: string;
+  size?: number;
+  strokeWidth?: number;
+  color?: string;
+  deg?: number;
+}) => {
+  const R = (size - strokeWidth) / 2;
+
+  const strokeLength = R * 2 * Math.PI * (deg / 360); // 45 度圆弧
+  const strokeSpace = R * 2 * Math.PI - strokeLength;
+
+  return (
+    <div
+      css={{
+        width: size,
+        height: size,
+      }}
+      className={className}
+    >
+      <svg
+        css={{
+          width: "100%",
+          height: "100%",
+        }}
+        viewBox={`0 0 ${size} ${size}`}
+      >
+        <circle
+          cx={size / 2}
+          cy={size / 2}
+          r={R}
+          stroke={color}
+          fill="none"
+          strokeWidth={strokeWidth}
+          strokeMiterlimit={10}
+          strokeLinecap="round"
+          strokeDasharray={strokeLength + "," + strokeSpace}
+        />
+      </svg>
+    </div>
+  );
+};
+
 export default () => {
-  return <div />;
+  const n = 6;
+  const maxDeg = 270;
+  const minDeg = maxDeg / n;
+  return (
+    <Container
+      css={css`
+        animation: ${kfRotate} 1.2s linear infinite;
+      `}
+    >
+      {Array(n)
+        .fill(0)
+        .map((_, i) => {
+          const kfRotateArc = keyframes`
+          0% {
+            transform: rotate(0deg);
+          }
+          50% {
+            transform: rotate(${i * minDeg}deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+          `;
+          return (
+            <Arc
+              key={i}
+              css={css`
+                position: absolute;
+                animation: ${kfRotateArc} 2.5s linear infinite;
+              `}
+              color={colors[i % colors.length]}
+              deg={minDeg}
+            />
+          );
+        })}
+    </Container>
+  );
 };
